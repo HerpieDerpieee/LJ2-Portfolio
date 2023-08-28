db9858c46adadf9667556e781f129f503cd83052
Valentijn :) <120188387+HerpieDerpieee@users.noreply.github.com>
2023-08-28 18:57:02 +0200

    template start

---
+++ Pages/homepage.php
@@ -0,0 +1,4 @@
+<div class="homepage-outer">
+    <div class="homepage-content"></div>
+    <div class="homepage-content"></div>
+</div>
--- index.php
+++ index.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="Styles/Index.css">
+    <link rel="stylesheet" href="Styles/Navigation.css">
+    <link rel="stylesheet" href="Styles/Homepage.css">
+    <link rel="stylesheet" href="Styles/AboutMe.css">
 </head>
 <body>
-    
+    <?php
+        include_once "Components/navigation.php";
+
+        include_once "Pages/homepage.php";
+        include_once "Pages/aboutme.php";
+    ?>
 </body>
 </html>
+
