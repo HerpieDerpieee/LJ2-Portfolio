9bdbbb3765d58841d3135697cea3e9aa12798f77
Valentijn :) <120188387+HerpieDerpieee@users.noreply.github.com>
2023-08-30 12:05:32 +0200

    added db connection and more page design

--- Pages/projects.php
+++ Pages/projects.php
@@ -1,3 +1,24 @@
 <div class="projects-outer">
-    projects lol
+    <div class="projects-grid" id="projects-container">
+        <?php
+        include_once "./Components/db.php";
+
+        $sql = "SELECT * FROM Portfolio_Projects";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                ?>
+                <div class="projects-content">
+                    <?= $row["name"] ?>
+                </div>
+                <?php
+            }
+        } else {
+            echo "0 results";
+        }
+
+        ?>
+    </div>
 </div>
