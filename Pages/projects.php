5feb052a10fca52133afa68c217c52d833b8f39c
Valentijn :) <120188387+HerpieDerpieee@users.noreply.github.com>
2023-09-01 14:36:06 +0200

    some minor changes

--- Pages/projects.php
+++ Pages/projects.php
@@ -1,4 +1,5 @@
 <div class="projects-outer" id="projects">
+    <h1 class="projects-fancy-title">Projecten</h1><br><br>
     <div class="projects-grid" id="projects-container">
         <?php
         include_once "./Components/db.php";
@@ -11,7 +12,7 @@
             while($row = $result->fetch_assoc()) {
                 ?>
                 <div class="projects-content">
-                    <div><?= $row["name"] ?></div>
+                    <h1><?= $row["name"] ?></h1>
                     <img class="projects-content-img" src="<?= $row['img'] ?>"/>
                     <div><?= $row["description"] ?></div>
                 </div>
@@ -25,4 +26,3 @@
     </div>
     <button id="projects-show-more" onclick="disappear(true)">Show More</button>
 </div>
-
