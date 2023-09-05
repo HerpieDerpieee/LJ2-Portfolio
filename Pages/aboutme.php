c07d04368b1db87994c9de9b3e5a49df866d144f
Valentijn :) <120188387+HerpieDerpieee@users.noreply.github.com>
2023-09-05 13:42:44 +0200

    added contact styling lol

--- Pages/aboutme.php
+++ Pages/aboutme.php
@@ -2,12 +2,12 @@
     <h1 class="pages-fancy-title">About Me</h1><br>
     <div class="aboutme-outer-flex">
         <div class="aboutme-content">
-            <span class="aboutme-text" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolores esse, maxime numquam pariatur perferendis sunt ullam voluptatum. Dignissimos distinctio, dolores enim fuga in inventore iusto odit perferendis repellendus vero?</span>
-        </div>
-        <div class="aboutme-content">
             <div class="pp-container">
                 <img src="./Media/aboutme-profile-picture.jpg" alt="proflile">
             </div>
+        </div>
+        <div class="aboutme-content">
+            <span class="aboutme-text" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolores esse, maxime numquam pariatur perferendis sunt ullam voluptatum. Dignissimos distinctio, dolores enim fuga in inventore iusto odit perferendis repellendus vero?</span>
         </div>
     </div>
 </div>
--- Pages/contact.php
+++ Pages/contact.php
@@ -1,11 +1,12 @@
 <div class="contact-outer" id="contact">
-    <form method="post" action="./Functions/insert.php">
-        <input required type="text" placeholder="Name" name="name">
-        <input required type="email" placeholder="E-mail adress " name="email">
-        <input required type="text" placeholder="Subject" name="subject">
-        <div class="form-div">
+    <h1 class="pages-fancy-title">Contact</h1><br><br>
+    <div class="contact-inner" id="projects-container">
+        <form method="post" action="./Functions/insert.php">
+            <input required type="text" placeholder="Name" name="name">
+            <input required type="email" placeholder="E-mail adress " name="email">
+            <input required type="text" placeholder="Subject" name="subject">
             <textarea required placeholder="Type your question here" name="message" id="form-content"></textarea>
-            <input type="submit" value="Submit"/>
-        </div>
-    </form>
+            <input type="submit" value="Submit" id="contact-form-submit-btn"/>
+        </form>
+    </div>
 </div>
