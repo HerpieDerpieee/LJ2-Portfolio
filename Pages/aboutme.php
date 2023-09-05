6efa4fd8ee2a2413563483f09d4735d45be8d12f
Valentijn :) <120188387+HerpieDerpieee@users.noreply.github.com>
2023-09-05 12:25:16 +0200

    chnages image size go brrrrrrrr

--- Pages/aboutme.php
+++ Pages/aboutme.php
@@ -1,11 +1,13 @@
 <div class="aboutme-outer" id="aboutme">
     <h1 class="pages-fancy-title">About Me</h1><br>
-    <div class="aboutme-content">
-        <span class="aboutme-text" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolores esse, maxime numquam pariatur perferendis sunt ullam voluptatum. Dignissimos distinctio, dolores enim fuga in inventore iusto odit perferendis repellendus vero?</span>
-    </div>
-    <div class="aboutme-content">
-        <div class="pp-container">
-            <img src="./Media/aboutme-profile-picture.jpg" alt="proflile">
+    <div class="aboutme-outer-flex">
+        <div class="aboutme-content">
+            <span class="aboutme-text" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolores esse, maxime numquam pariatur perferendis sunt ullam voluptatum. Dignissimos distinctio, dolores enim fuga in inventore iusto odit perferendis repellendus vero?</span>
+        </div>
+        <div class="aboutme-content">
+            <div class="pp-container">
+                <img src="./Media/aboutme-profile-picture.jpg" alt="proflile">
+            </div>
         </div>
     </div>
 </div>
